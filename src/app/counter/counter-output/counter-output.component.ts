819cc72fb476e1ed8cd4d8d5c1d88fd2a3821455
Harsha Royal <harshasrit223@gmail.com>
2022-11-14 01:16:41 +0530

    createFeatureSelector and createFeature

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { getCounter } from '../state/counter.selector';
 import { CounterState } from '../state/counter.state';
 @Component({
   selector: 'app-counter-output',
@@ -9,12 +10,12 @@ import { CounterState } from '../state/counter.state';
 })
 export class CounterOutputComponent implements OnInit {
 
-  counter$ : Observable<{counter:number }>;
+  counter$ : Observable<number>;
 
   constructor(private store : Store<{counter:CounterState}>) { }
 
   ngOnInit(): void {
-      this.counter$ = this.store.select('counter')
+      this.counter$ = this.store.select(getCounter)
   }
 
 
--- src/app/counter/custom-counter-input/custom-counter-input.component.ts
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { channelNameChange, customIncrement } from '../state/counter.action';
+import { getChannelName, getCounter } from '../state/counter.selector';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -12,13 +14,9 @@ export class CustomCounterInputComponent implements OnInit {
   value : number;
   channelName : string;
   constructor(private store : Store<{counter:CounterState}>) { }
-
+  channelName$ : Observable<string>;
   ngOnInit(): void {
-    this.store.select('counter').subscribe((data)=>
-    {
-
-      this.channelName = data.channelName;
-    })
+    this.channelName$ = this.store.select(getChannelName);
   }
 
   onAdd(){
